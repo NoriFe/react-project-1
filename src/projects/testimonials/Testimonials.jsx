013a5d0ee3ff160415c698d8bd4be73b521fadf0
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2024-06-15 14:17:27 +0100

    Fetching testimonials using useEffect in React

--- src/projects/testimonials/Testimonials.jsx
+++ src/projects/testimonials/Testimonials.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Title from '../components/Title'
 import Button from '../components/Button'
 import { BsFillFileEarmarkPostFill } from 'react-icons/bs'
@@ -6,15 +6,41 @@ import { FaUserAlt } from 'react-icons/fa'
 import { BiCommentDetail } from 'react-icons/bi'
 
 export default function Testimonials() {
-    const handleClick = () => {
+    // const handleClick = () => {
         
-    }
+    // }
+    const [testimonials, setTestimonials] = useState("");
+    const [items, setItems] = useState();
+
+    useEffect(() => {
+      fetch(`https://jsonplaceholder.typicode.com/${testimonials}`)
+        .then((response) => response.json())
+        .then((json) => setItems(json));
+    }, [testimonials]);
+
   return (
-    <div className='cointainer m-auto'>
+    <div className='cointainer m-auto text-center'>
       <Title text={"Testimonials"} />
-      <Button text={" Posts"} btnClass="btn-info" icon={<BsFillFileEarmarkPostFill/>} onClick={handleClick}/>
-      <Button text={" Users"} btnClass="btn-info" icon={<FaUserAlt />} onClick={handleClick}/>
-      <Button text={" Commnets"} btnClass="btn-info" icon={<BiCommentDetail/>} onClick={handleClick}/>
+      <Button text={" Posts"} btnClass="btn-info" icon={<BsFillFileEarmarkPostFill/>} onClick={()=> setTestimonials("Posts")}/>
+      <Button text={" Users"} btnClass="btn-info" icon={<FaUserAlt />} onClick={()=> setTestimonials("Users")}/>
+      <Button text={" Commnets"} btnClass="btn-info" icon={<BiCommentDetail/>} onClick={()=> setTestimonials("Comments")}/>
+      <Title classes={"subtitle text-primary"} text={!testimonials ? "Testimonials details" : testimonials} />
+      {!items
+        ? null
+        : items.map((item) => {
+            return (
+              <div className="card card-primary mb-2" key={item.id}>
+                {item.name && <h2 className="card-header">{item.name}</h2>}
+                <div className="card-body">
+                  <h4>{item.title}</h4>
+                  <p>{item.body}</p>
+                </div>
+                {item.email && (
+                  <small className="card-footer">{item.email}</small>
+                )}
+              </div>
+            );
+          })}
     </div>
-  )
+  );
 }
