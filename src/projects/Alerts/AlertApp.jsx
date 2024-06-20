718090381ec9d2d281f4a7bceb503127c3e340da
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2024-06-20 11:53:47 +0100

    AlertApp component with multiple Alert examples

--- src/projects/Alerts/AlertApp.jsx
+++ src/projects/Alerts/AlertApp.jsx
@@ -5,6 +5,9 @@ export default function AlertApp() {
   return (
     <div className='cointainer mx-auto'>    
       <Alert type={"success"} message={"Login successful"}/>
+      <Alert type={"danger"} message={"Login failed"}/>
+      <Alert type={"warning"} message={"Please fill out all fields"}/>
+      <Alert type={"info"} message={"Please check your email"} delay={true}/>
     </div>
   )
 }
--- src/projects/components/Alert.jsx
+++ src/projects/components/Alert.jsx
@@ -1,12 +1,23 @@
 import React, { useState, useEffect } from 'react'
 import Button from './Button'
 
-export default function Alert({type, message}) {
+export default function Alert({type, message, delay = false}) {
     const [alert, setAlert] = useState(true)
+    const closeAlert = (e) => {
+        e.target.parentElement.parentElement.classList.add("fadeAlert")
+        setTimeout(() => {
+            setAlert(false)
+        }, 400)
+    }
+        useEffect(() => {
+            delay && setTimeout(() => {
+                setAlert(false)
+            }, 3000)
+        })
   return alert && <div className={`alert alert-${type}`}>
     <div className="alert-close">
         <span className="mr-1">{message}</span>
-        <Button />
+        <Button btnClass={"btn-close"} text={"X"} onClick={closeAlert}/>
 
     </div>
   </div>  
